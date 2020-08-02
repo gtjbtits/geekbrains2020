c57fbb04955a1091294915e22a5bdd32deda74d0
Nikolay Zaytsev <madcmk2@gmail.com>
2020-08-03 00:11:15 +0300

    Гарантированная реализация семафора в Tunnel через finally

--- lv3lesson5/src/main/java/com/jbtits/gb/lv3/lesson5/stages/Tunnel.java
+++ lv3lesson5/src/main/java/com/jbtits/gb/lv3/lesson5/stages/Tunnel.java
@@ -11,10 +11,10 @@ import java.util.concurrent.Semaphore;
  */
 public class Tunnel extends Stage {
 
-    private final Semaphore capacityController;
+    private final Semaphore capacitySemaphore;
 
     public Tunnel(int length, int partiesCount) {
-        this.capacityController = new Semaphore(partiesCount / 2, true);
+        this.capacitySemaphore = new Semaphore(partiesCount / 2, true);
         this.description = "Тоннель " + length + " метров";
         this.length = length;
     }
@@ -22,13 +22,16 @@ public class Tunnel extends Stage {
     @Override
     @SneakyThrows
     public void go(Car c) {
-        if (capacityController.availablePermits() < 1) {
+        if (capacitySemaphore.availablePermits() < 1) {
             System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
         }
-        capacityController.acquire();
-        System.out.println(c.getName() + " начал этап: " + description);
-        Thread.sleep(length / c.getSpeed() * 1000);
-        System.out.println(c.getName() + " закончил этап: " + description);
-        capacityController.release();
+        capacitySemaphore.acquire();
+        try {
+            System.out.println(c.getName() + " начал этап: " + description);
+            Thread.sleep(length / c.getSpeed() * 1000);
+            System.out.println(c.getName() + " закончил этап: " + description);
+        } finally {
+            capacitySemaphore.release();
+        }
     }
 }
